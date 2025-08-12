986027c083a321f62e1976b4befaf68c7183ca87
imattau <imatt.au@protonmail.com>
2025-08-12 22:47:16 +1000

    Add video overlay and navigation bar

---
+++ src/components/nav/BottomNav.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+
+/**
+ * Fixed bottom navigation bar with primary app sections.
+ * Purely presentational; parent pages handle routing context.
+ */
+export default function BottomNav() {
+  return (
+    <nav className="pointer-events-auto fixed bottom-0 left-0 right-0 flex justify-around bg-black/60 p-2 text-white text-xs">
+      <Link href="/home" className="flex flex-col items-center">
+        Home
+      </Link>
+      <Link href="/upload" className="flex flex-col items-center">
+        Upload
+      </Link>
+      <Link href="/profile" className="flex flex-col items-center">
+        Profile
+      </Link>
+    </nav>
+  );
+}
